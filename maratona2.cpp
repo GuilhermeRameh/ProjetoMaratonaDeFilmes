8f561f94c1dca414293f4ea3888fb4b2366a27e5
GuilhermeRameh <guilhermedr@al.insper.edu.br>
2023-05-30 17:38:49 -0300

    Feito openmp

--- maratona2.cpp
+++ maratona2.cpp
@@ -38,8 +38,8 @@ void Exaustiva()
 
     bitset<64> bitsetFilmes(0);
 
-    filmesPorCats.reserve(ncategorias);
-    fpcReset.reserve(ncategorias);
+    filmesPorCats.resize(ncategorias);
+    fpcReset.resize(ncategorias);
 
     // TODO: PARALELIZAR
     for (int i=0; i<ncategorias; i++)
@@ -132,27 +132,23 @@ void Exaustiva()
 
 void ParOPENMP()
 {
-    int nfilmes, ncategorias, total, comp_total = 0;
+    int nfilmes, ncategorias, comp_total = 0;
     cin >> nfilmes >> ncategorias;
 
-    vector<int> filmesPorCats, fpcReset;
+    vector<int> filmesPorCats, fpcReset, total_values;
     vector<filme> filmes;
 
-    bitset<24> horas(0x0000000000000000000);
+    filmesPorCats.resize(ncategorias);
+    fpcReset.resize(ncategorias);
 
-    bitset<64> bitsetFilmes(0);
-
-    filmesPorCats.reserve(ncategorias);
-    fpcReset.reserve(ncategorias);
-
-    #pragma omp parallel for
+    // NOTE: Não paralelizavel pois possui o CIN
     for (int i=0; i<ncategorias; i++)
     {
         cin >> filmesPorCats[i];
         fpcReset[i] = filmesPorCats[i];
     }
 
-    // TODO: PARALELIZAR
+    // NOTE: Não paralelizavel pois possui o CIN
     for (int i=0; i<nfilmes; i++)
     {
         filme this_film;
@@ -160,21 +156,28 @@ void ParOPENMP()
         cin >> this_film.hInicio >> this_film.hFinal >> this_film.categoria;
         filmes.push_back(this_film);
     }
-
     
     // NOTE: Aqui é o for de 0 a 2^filmes - faz todas as possibilidades
-    // TODO: PARALELIZAR
-
-    for (int i=0;i<pow(2, nfilmes); i++)
+    int nExpo = pow(2, nfilmes);
+ 
+    #pragma omp parallel for reduction(max : comp_total)
+    for (int i=0;i<nExpo; i++)
     {
-        total = 0;
+        bitset<24> horas(0x0000000000000000000);
+        bitset<64> bitsetFilmes(0);
+        vector<int> localfilmesPorCats;
+        localfilmesPorCats.resize(ncategorias);
+
+        int local_total_iteration = 0;
         bitsetFilmes = i;
         horas = 0;
 
         for (int i=0; i<ncategorias; i++)
         {
-            filmesPorCats[i] = fpcReset[i] ;
+            localfilmesPorCats[i] = fpcReset[i] ;
         }
+
+        // cout << omp_get_thread_num() << " " << i << endl;
 
         for (int j=0; j<nfilmes; j++)
         {            
@@ -183,7 +186,7 @@ void ParOPENMP()
                 filme valor = filmes[j];
                 
                 if (valor.hInicio <= valor.hFinal){
-                    if (filmesPorCats[valor.categoria] <= 0)
+                    if (localfilmesPorCats[valor.categoria] <= 0)
                     {
                         break;
                     }
@@ -201,18 +204,110 @@ void ParOPENMP()
                         for (int i = valor.hInicio; i <= valor.hFinal; i++) {
                             horas.set(i);
                         }
-                        filmesPorCats[valor.categoria]--;
-                        total += 1;
+                        localfilmesPorCats[valor.categoria]--;
+                        local_total_iteration += 1;
                         // cout << "  > Iter - " << i << "   ID: " << valor.id << endl;
                     }
                 }
             }
         }
-        if (total > comp_total)
+        if (local_total_iteration > comp_total)
         {
-            comp_total = total;
+            comp_total = local_total_iteration;
         }
+    }  
+
+    cout << "\nTOTAL GLOBAL: " << comp_total << endl;
+}
+
+
+
+void ParTHRUST()
+{
+    int nfilmes, ncategorias, comp_total = 0;
+    cin >> nfilmes >> ncategorias;
+
+    vector<int> filmesPorCats, fpcReset, total_values;
+    vector<filme> filmes;
+
+    filmesPorCats.resize(ncategorias);
+    fpcReset.resize(ncategorias);
+
+    // NOTE: Não paralelizavel pois possui o CIN
+    for (int i=0; i<ncategorias; i++)
+    {
+        cin >> filmesPorCats[i];
+        fpcReset[i] = filmesPorCats[i];
     }
+
+    // NOTE: Não paralelizavel pois possui o CIN
+    for (int i=0; i<nfilmes; i++)
+    {
+        filme this_film;
+        this_film.id = i;
+        cin >> this_film.hInicio >> this_film.hFinal >> this_film.categoria;
+        filmes.push_back(this_film);
+    }
+    
+    // NOTE: Aqui é o for de 0 a 2^filmes - faz todas as possibilidades
+    int nExpo = pow(2, nfilmes);
+ 
+    #pragma omp parallel for reduction(max : comp_total)
+    for (int i=0;i<nExpo; i++)
+    {
+        bitset<24> horas(0x0000000000000000000);
+        bitset<64> bitsetFilmes(0);
+        vector<int> localfilmesPorCats;
+        localfilmesPorCats.resize(ncategorias);
+
+        int local_total_iteration = 0;
+        bitsetFilmes = i;
+        horas = 0;
+
+        for (int i=0; i<ncategorias; i++)
+        {
+            localfilmesPorCats[i] = fpcReset[i] ;
+        }
+
+        // cout << omp_get_thread_num() << " " << i << endl;
+
+        for (int j=0; j<nfilmes; j++)
+        {            
+            if (bitsetFilmes[j])
+            {
+                filme valor = filmes[j];
+                
+                if (valor.hInicio <= valor.hFinal){
+                    if (localfilmesPorCats[valor.categoria] <= 0)
+                    {
+                        break;
+                    }
+                    bitset<24> mascara;
+                    for (int i = valor.hInicio; i <= valor.hFinal; i++) {
+                        mascara.set(i);
+                    }
+
+                    // cout << "\nInicio: " << valor.hInicio << " Final: " << valor.hFinal;
+
+                    bitset<24> resultado = horas & mascara;
+
+                    if (resultado == 0)
+                    {
+                        for (int i = valor.hInicio; i <= valor.hFinal; i++) {
+                            horas.set(i);
+                        }
+                        localfilmesPorCats[valor.categoria]--;
+                        local_total_iteration += 1;
+                        // cout << "  > Iter - " << i << "   ID: " << valor.id << endl;
+                    }
+                }
+            }
+        }
+        if (local_total_iteration > comp_total)
+        {
+            comp_total = local_total_iteration;
+        }
+    }  
 
     cout << "\nTOTAL GLOBAL: " << comp_total << endl;
 }
@@ -222,8 +317,8 @@ int main()
 {
     
     // Exaustiva();
-    ParOPENMP();
-    // ParTHRUST();
+    // ParOPENMP();
+    ParTHRUST();
     
     return 0;
 }
